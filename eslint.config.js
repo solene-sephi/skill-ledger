9c4a6dfb49ed7b3a46e0d1940e80780b707b58f4
Solene Brunel <brunel.solene.dev@gmail.com>
2025-11-24 16:34:51 +0100

    🔧 Enable type-aware ESLint config and add React, CSS modules and promise plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,46 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
+import promise from "eslint-plugin-promise";
+import cssModules from "eslint-plugin-css-modules";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      // Enable type-aware lint rules
+      tseslint.configs.recommendedTypeChecked,
+      // Enable stylistic lint rules
+      tseslint.configs.stylisticTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      // Enable lint rules for React
+      reactX.configs["recommended-typescript"],
+      // Enable lint rules for React DOM
+      reactDom.configs.recommended,
     ],
+    plugins: {
+      promise,
+      "css-modules": cssModules,
+    },
+    rules: {
+      ...promise.configs.recommended.rules,
+      ...cssModules.configs.recommended.rules,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
-])
+]);
