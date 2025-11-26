aca3d04d4e0d7a85264eb20ede4a24efd30f5a73
Solene Brunel <brunel.solene.dev@gmail.com>
2025-11-26 13:57:53 +0100

    ✨ feat: display skill cards on dashboard

---
+++ src/components/SkillCard.tsx
@@ -0,0 +1,22 @@
+import type { Skill } from "../types/Skill";
+
+interface SkillCardProps {
+  skill: Skill;
+}
+
+export default function SkillCard({ skill }: SkillCardProps) {
+  return (
+    <div className="card w-[500px] rounded border border-fuchsia-900 px-4 py-2">
+      <div className="card-body">
+        <h2 className="card-title font-bold">{skill.name}</h2>
+        <p>Type : {skill.type}</p>
+        <p>Nombre d'actions : {skill.actionNb}</p>
+        <div className="flex justify-end">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+            Détails
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,5 +1,10 @@
 import { Outlet } from "react-router";
 
 export default function RootLayout() {
-  return <Outlet />;
+  return (
+    <div className="min-h-screen bg-gray-200 px-4 pt-4 pb-8">
+      <h1>Skill Ledger</h1>
+      <Outlet />
+    </div>
+  );
 }
