72344092e816343b42f6d705cd801987b59caf47
Solene Brunel <brunel.solene.dev@gmail.com>
2025-12-16 08:49:44 +0100

    ✨ feat: add link to dashboard in header

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,10 +1,12 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 
 export default function RootLayout() {
   return (
     <>
       <header className="px-4 py-6 sm:px-6 border-b border-grey-500">
-        <h1 className="text-3xl font-bold font-ubuntu">Skill Ledger</h1>
+        <Link className="text-3xl font-bold font-ubuntu" to={`/`}>
+          Skill Ledger
+        </Link>
       </header>
       <main className="min-h-screen">
         <Outlet />
